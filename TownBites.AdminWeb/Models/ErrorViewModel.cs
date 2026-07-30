e7cb2315dbc8b5c3da97431553db7beb18a7523b
psiva246 <psiva246@gmail.com>
2026-07-30 13:55:11 +0530

    feat(admin): initialize MVC project

---
+++ TownBites.AdminWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TownBites.AdminWeb.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
